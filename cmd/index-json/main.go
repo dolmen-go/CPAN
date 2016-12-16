a8b3347c1fe039b551f3c1bd43d7c8766d1ecd2c
Olivier Mengué <dolmen@cpan.org>
2016-12-17 00:33:56 +0100

    Add cmd/index-json: demo of ReadIndex()

---
+++ cmd/index-json/main.go
@@ -0,0 +1,42 @@
+// An example of CPAN.ReadIndex(): converts a 02packages.tar.gz to JSON.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/dolmen-go/CPAN"
+)
+
+func main() {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	_, entries, done := CPAN.ReadIndex(f)
+
+	sep := "[\n"
+LOOP:
+	for {
+		select {
+		case entry := <-entries:
+			os.Stdout.WriteString(sep)
+			sep = ",\n"
+			buf, _ := json.Marshal(entry)
+			os.Stdout.Write(buf)
+		case err = <-done:
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			break LOOP
+		}
+	}
+	if sep[0] != '[' {
+		os.Stdout.Write([]byte{']', '\n'})
+	}
+}
